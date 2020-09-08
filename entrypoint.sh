7163a868834a4e04c93ffba6a277b4a7f9ab731d
boredazfcuk <boredazfcuk@hotmail.co.uk>
2020-09-08 18:13:53 +0100

    Please check change.log

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,7 +11,7 @@ Initialise(){
    echo "$(date '+%d/%m/%Y %H:%M:%S')| IP address: ${lan_ip}"
    echo "$(date '+%d/%m/%Y %H:%M:%S')| Container network: ${container_network}"
    echo "$(date '+%d/%m/%Y %H:%M:%S')| Config directory: ${config_dir}"
-   echo "$(date '+%d/%m/%Y %H:%M:%S')| Cache directory: ${cache_dir}"
+   echo "$(date '+%d/%m/%Y %H:%M:%S')| Data directory: ${data_dir:=/data}"
    if [ "${home_dir}" ]; then echo "$(date '+%d/%m/%Y %H:%M:%S')| Home directory for proxyconfig web server to serve proxy pac and installation certificates: ${home_dir}"; fi
 }
 
@@ -32,8 +32,7 @@ FirstRun(){
             "${config_dir}/squid.conf"
          echo "$(date '+%d/%m/%Y %H:%M:%S')| Create default peek and splice configuration"
          peek_and_splice="$(echo "https_port ${lan_ip}:3129 intercept ssl-bump cert=${config_dir}/https/squid_ca_chain.pem generate-host-certificates=on dynamic_cert_mem_cache_size=16MB" \
-            "\nsslcrtd_program /usr/lib/squid/security_file_certgen -d -s ${config_dir}/ssl_db -M 4MB" \
-            "\nsslcrtd_children 3 startup=1 idle=1" \
+            "\nsslcrtd_program /usr/lib/squid/security_file_certgen -d -s ${data_dir}/ssl_db -M 16MB" \
             "\nacl step1 at_step SslBump1" \
             "\nssl_bump peek step1" \
             "\nssl_bump bump all" \
@@ -70,25 +69,27 @@ FirstRun(){
                echo 'issuerAltName          = issuer:copy'
             } >"${config_dir}/https/ca.cnf"
          fi
-
          if [ ! -f "${config_dir}/https/squid_ca_key.pem" ] || [ ! -f "${config_dir}/https/squid_ca_cert.pem" ]; then
             echo "$(date '+%d/%m/%Y %H:%M:%S')| Generating certification authority certificates"
             openssl req -new -newkey rsa:2048 -sha256 -days 3650 -nodes -x509 -extensions v3_ca -config "${config_dir}/https/ca.cnf" -subj "/C=NA/ST=Global/L=Global/O=Squid/OU=Squid/CN=Squid/" -keyout "${config_dir}/https/squid_ca_key.pem"  -out "${config_dir}/https/squid_ca_cert.pem"
             echo "$(date '+%d/%m/%Y %H:%M:%S')| Creating chained certification authority certificate"
             cat "${config_dir}/https/squid_ca_cert.pem" "${config_dir}/https/squid_ca_key.pem" >> "${config_dir}/https/squid_ca_chain.pem"
          fi
-
          if [ ! -f "${config_dir}/https/squid_ca_cert.der" ]; then
             echo "$(date '+%d/%m/%Y %H:%M:%S')| Converting certification authority certificate to DER format for client installation"
             openssl x509 -in "${config_dir}/https/squid_ca_cert.pem" -outform DER -out "${config_dir}/https/squid_ca_cert.der"
          fi
-
-         if [ ! -d "${config_dir}/ssl_db" ]; then
-            echo "$(date '+%d/%m/%Y %H:%M:%S')| Creating database for dynamically generated certificates"
-            /usr/lib/squid/security_file_certgen -c -d -s "${config_dir}/ssl_db" -M 4MB
+         if [ -d "${data_dir}/ssl_db" ]; then
+            echo "$(date '+%d/%m/%Y %H:%M:%S')| Removing SSL certificate DB as new CA certificate generated"
+            rm -r "${data_dir}/ssl_db"
          fi
       fi
-
+      if [ ! -d "${data_dir}/ssl_db" ]; then
+         echo "$(date '+%d/%m/%Y %H:%M:%S')| Creating SSL certificate DB directory"
+         mkdir --parents "${data_dir}/ssl_db"
+         /usr/lib/squid/security_file_certgen -c -s ${data_dir}/ssl_db -M 16MB
+         chown squid:squid "${data_dir}/ssl_db"
+      fi
       echo "$(date '+%d/%m/%Y %H:%M:%S')| Configure logging"
       {
          echo
@@ -125,18 +126,6 @@ FirstRun(){
          echo 'pid_filename /run/squid/${service_name}.pid'
       } >> "${config_dir}/squid.conf"
 
-      if [ "$(grep -c "acl ignore_container_network src" "${config_dir}/squid.conf")" -eq 0 ]; then
-         echo "$(date '+%d/%m/%Y %H:%M:%S')| Create access control list to prevent logging for container network"
-         sed -i \
-            -e "/RFC 1122/i acl ignore_container_network src ${container_network}" \
-            "${config_dir}/squid.conf"
-      fi
-      if [ "$(grep -c "acl ignore_healthcheck has request" "${config_dir}/squid.conf")" -eq 0 ]; then
-         echo "$(date '+%d/%m/%Y %H:%M:%S')| Create access control list to ignore healthchecks (empty requests)"
-         sed -i \
-            -e "/RFC 1122/i acl ignore_healthcheck has request" \
-            "${config_dir}/squid.conf"
-      fi
       echo "$(date '+%d/%m/%Y %H:%M:%S')| First run configuration complete"
       rm "/first_run"
    fi
@@ -144,9 +133,9 @@ FirstRun(){
 
 Configure(){
    if [ "$(grep -c "acl ignore_container_network src" "${config_dir}/squid.conf")" -eq 0 ]; then
-      echo "$(date '+%d/%m/%Y %H:%M:%S')| Create access control list to prevent logging for container network"
+      echo "$(date '+%d/%m/%Y %H:%M:%S')| Create access control list to prevent logging for container network: ${container_network}"
       sed -i \
-         -e "/RFC 1122/i acl ignore_container_network src" \
+         -e "/RFC 1122/i acl ignore_container_network src ${container_network}" \
          "${config_dir}/squid.conf"
    fi
    if [ "$(grep -c "acl ignore_healthcheck has request" "${config_dir}/squid.conf")" -eq 0 ]; then
@@ -157,10 +146,10 @@ Configure(){
    fi
    echo "$(date '+%d/%m/%Y %H:%M:%S')| Configure cache directory"
    sed -i \
-      -e "s%^coredump_dir .*%coredump_dir ${cache_dir}%" \
-      -e "s%#cache_dir .*%cache_dir aufs ${cache_dir} 3072 16 256%" \
+      -e "s%^coredump_dir .*%coredump_dir ${data_dir}/cache%" \
+      -e "s%#cache_dir .*%cache_dir aufs ${data_dir}/cache 3072 16 256%" \
       "${config_dir}/squid.conf"
-   echo "$(date '+%d/%m/%Y %H:%M:%S')| Set Squid's LAN IP subnet in access control list"
+   echo "$(date '+%d/%m/%Y %H:%M:%S')| Set Squid's LAN IP subnet in access control list: ${container_network}"
    sed -i \
       -e "s%^acl ignore_container_network src.*%acl ignore_container_network src ${container_network}%" \
       "${config_dir}/squid.conf"
@@ -173,14 +162,18 @@ Configure(){
       -e "s%^https_port .*%https_port ${lan_ip}:3129 intercept ssl-bump cert=${config_dir}/https/squid_ca_chain.pem generate-host-certificates=on dynamic_cert_mem_cache_size=16MB%" \
       "${config_dir}/squid.conf"
    if [ -d "${home_dir}" ]; then
-      echo "$(date '+%d/%m/%Y %H:%M:%S')| HTTPd server home folder detected, copying certificates"
+      echo "$(date '+%d/%m/%Y %H:%M:%S')| HTTPd server home folder detected, copying certificates to: ${home_dir}"
       cp "${config_dir}/https/squid_ca_cert.pem" "${config_dir}/https/squid_ca_cert.der" "${home_dir}"
    fi
-   if [ ! -f "${cache_dir}/swap.state" ]; then
-      echo "$(date '+%d/%m/%Y %H:%M:%S')| Creating cache for dynamically generated certificates"
-      chown -R squid:squid "${cache_dir}"
-      "$(which squid)" -N -z -f "${config_dir}/squid.conf"
+   if [ "$(ls -A "${data_dir}/cache" 2>/dev/null)" != "" ]; then
+      echo "$(date '+%d/%m/%Y %H:%M:%S')| Remove cache directories"
+      rm -r "${data_dir}/cache/0"* 2>/dev/null
    fi
+   echo "$(date '+%d/%m/%Y %H:%M:%S')| Remove cache swap state files"
+   if [ -f "${data_dir}/cache/swapstate" ]; then rm "${data_dir}/cache/swapstate"; fi
+   if [ -f "${data_dir}/cache/swapstate.new" ]; then rm "${data_dir}/cache/swapstate.new"; fi
+   echo "$(date '+%d/%m/%Y %H:%M:%S')| Initialise cache for dynamically generated certificates"
+   /usr/sbin/squid -N -z -f "${config_dir}/squid.conf"
 }
 
 SetOwnerAndGroup(){
@@ -201,7 +194,7 @@ LaunchSquid (){
    echo "$(date '+%d/%m/%Y %H:%M:%S')| ***** Configuration of Squid container launch environment complete *****"
    if [ -z "${1}" ]; then
       echo "$(date '+%d/%m/%Y %H:%M:%S')| Starting Squid"
-      exec "$(which squid)" -NYC -d9 -f "${config_dir}/squid.conf"
+      exec /usr/sbin/squid -NYCD -d9 -f "${config_dir}/squid.conf"
    else
       exec "$@"
    fi
